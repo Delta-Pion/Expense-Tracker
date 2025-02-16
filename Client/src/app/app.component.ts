528b20b26fd653db770451d9f400115dd0205716
Delta-Pion <adi.28per@gmail.com>
2025-02-16 23:21:07 +0530

    Client - Routing established

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { ButtonModule } from 'primeng/button';
-import { RegisterComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, NavComponent, RegisterComponent],
+  imports: [RouterOutlet, ButtonModule, NavComponent, SignupComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  //encapsulation : ViewEncapsulation.None
 })
 export class AppComponent {
-  title : string = 'Practice';
+  title : string = 'Client';
 
   
 }
