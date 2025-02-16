551fff0613c0e86019c12b2a203eef9f8f1f3730
Delta-Pion <adi.28per@gmail.com>
2025-02-16 23:20:20 +0530

    Client - Signup Component Imroved

---
+++ Client/src/app/signup/signup.component.ts
@@ -0,0 +1,37 @@
+import { Component,OnInit } from '@angular/core';
+import { Checkbox } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { PasswordModule } from 'primeng/password';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [Checkbox,InputTextModule,ButtonModule,CardModule,FormsModule,ReactiveFormsModule,FloatLabelModule,PasswordModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent implements OnInit{
+
+  signUpForm = new FormGroup({
+    username: new FormControl<string | null>(null),
+    email: new FormControl<string | null>(null),
+    password: new FormControl<string | null>(null)
+  });
+
+  ngOnInit() {
+        
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm?.value);
+    this.signUpForm.get('username')?.setValue("");
+    this.signUpForm.get('email')?.setValue("");
+    this.signUpForm.get('password')?.setValue("");
+  }
+}
+
