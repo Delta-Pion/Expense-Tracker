07fb63ed81b754a45b4eb89e7c1b4b8c6fb7473a
Delta-Pion <adi.28per@gmail.com>
2025-02-19 23:05:56 +0530

    Client - CORS support added for client uri

--- ETAPI/Program.cs
+++ ETAPI/Program.cs
@@ -34,6 +34,8 @@ if (app.Environment.IsDevelopment())
 
 app.MapIdentityApi<User>();
 
+app.UseCors( x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
