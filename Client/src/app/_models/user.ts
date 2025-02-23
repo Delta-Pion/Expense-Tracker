09f062b2eb8e12a13e3ce7ece459337fd76c5041
Delta-Pion <adi.28per@gmail.com>
2025-02-23 21:59:36 +0530

    Client - Updated User Model to user dto

--- Client/src/app/_models/user.ts
+++ Client/src/app/_models/user.ts
@@ -1,18 +1,7 @@
 export interface User {
-    expenses: any[]
+
     id: string
     userName: string
-    normalizedUserName: string
-    email: string
-    normalizedEmail: string
-    emailConfirmed: boolean
-    passwordHash: string
-    securityStamp: string
-    concurrencyStamp: string
-    phoneNumber: any
-    phoneNumberConfirmed: boolean
-    twoFactorEnabled: boolean
-    lockoutEnd: any
-    lockoutEnabled: boolean
-    accessFailedCount: number
+    knownAs: string
+    
   }
