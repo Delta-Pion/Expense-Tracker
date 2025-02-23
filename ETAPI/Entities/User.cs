91e367e62d72fa3e2d96f6e0a8155f695bbac8c4
Delta-Pion <adi.28per@gmail.com>
2025-02-23 20:49:14 +0530

    API - Known As property added

--- ETAPI/Entities/User.cs
+++ ETAPI/Entities/User.cs
@@ -5,5 +5,6 @@ namespace ETAPI.Entities;
 
 public class User : IdentityUser
 {
+    public string KnownAs { get; set; } = "AxT USER";
     public ICollection<Expense> Expenses{ get; set; } = [];
 }
