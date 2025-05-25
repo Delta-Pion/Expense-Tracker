468c5822955c4cf8b08a8bc72f8a099aea19b60f
Delta-Pion <adi.28per@gmail.com>
2025-05-25 13:50:27 +0530

    Nav component now has link for home page

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -6,90 +6,92 @@ import { AvatarModule } from 'primeng/avatar';
 import { CommonModule, NgIf } from '@angular/common';
 import { Menu } from 'primeng/menu';
 import { AccountService } from '../_services/account.service';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [Menubar,BadgeModule,AvatarModule,CommonModule,NgIf,Menu],
+  imports: [Menubar, BadgeModule, AvatarModule, CommonModule, NgIf, Menu, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-    private accountService = inject(AccountService);
+  private accountService = inject(AccountService);
 
   items: MenuItem[] | undefined;
 
   profileItems: MenuItem[] | undefined;
 
   ngOnInit() {
-      this.items = [
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        route: "home"
+      },
+      // {
+      //     label: 'Features',
+      //     icon: 'pi pi-star'
+      // },
+      {
+        label: 'Transactions',
+        icon: 'pi pi-credit-card',
+        items: [
           {
-              label: 'Home',
-              icon: 'pi pi-home'
-          },
-          // {
-          //     label: 'Features',
-          //     icon: 'pi pi-star'
-          // },
-          {
-              label: 'Transactions',
-              icon: 'pi pi-credit-card',
-              items: [
-                  {
-                      label: 'Income',
-                      icon: 'pi pi-dollar'
-                  },
-                  {
-                      label: 'Expenses',
-                      icon: 'pi pi-wallet'
-                  },
-                  {
-                    label: 'All',
-                    icon : 'pi pi-list'
-                  },
-                  {
-                      label: 'Bills',
-                      icon: 'pi pi-receipt'
-                  },
-                  
-                  // {
-                  //     label: 'Templates',
-                  //     icon: 'pi pi-palette',
-                  //     items: [
-                  //         {
-                  //             label: 'Apollo',
-                  //             icon: 'pi pi-palette'
-                  //         },
-                  //         {
-                  //             label: 'Ultima',
-                  //             icon: 'pi pi-palette'
-                  //         }
-                  //     ]
-                  // }
-              ]
+            label: 'Income',
+            icon: 'pi pi-dollar'
           },
           {
-              label: 'Contact',
-              icon: 'pi pi-envelope'
-          }
-      ]
+            label: 'Expenses',
+            icon: 'pi pi-wallet'
+          },
+          {
+            label: 'All',
+            icon: 'pi pi-list'
+          },
+          {
+            label: 'Bills',
+            icon: 'pi pi-receipt'
+          },
 
-      this.profileItems = [
-        {
-            label: 'Profile',
-                items: [
-                    {
-                        label: 'Settings',
-                        icon: 'pi pi-cog'
-                    },
-                    {
-                        label: 'Logout',
-                        icon: 'pi pi-sign-out',
-                        command: () => {
-                            this.accountService.logout();
-                        }
-                    }
-                ]
-        }
+          // {
+          //     label: 'Templates',
+          //     icon: 'pi pi-palette',
+          //     items: [
+          //         {
+          //             label: 'Apollo',
+          //             icon: 'pi pi-palette'
+          //         },
+          //         {
+          //             label: 'Ultima',
+          //             icon: 'pi pi-palette'
+          //         }
+          //     ]
+          // }
+        ]
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope'
+      }
+    ]
+
+    this.profileItems = [
+      {
+        label: 'Profile',
+        items: [
+          {
+            label: 'Settings',
+            icon: 'pi pi-cog'
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            command: () => {
+              this.accountService.logout();
+            }
+          }
+        ]
+      }
     ];
   }
 }
