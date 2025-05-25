4f7f505f088167b6eda140e23e446c69f8974b51
Delta-Pion <adi.28per@gmail.com>
2025-05-25 13:51:41 +0530

    Routes added for all the new pages

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AllTransactionsComponent } from './all-transactions/all-transactions.component';
 
 export const routes: Routes = [
-    {path: "signup" , component : SignupComponent},
-    {path: "" , component : LoginComponent}
+  { path: "signup", component: SignupComponent },
+  { path: "", component: LoginComponent },
+  { path: "home", component: HomeComponent },
+  { path: "alltransactions", component: AllTransactionsComponent }
 ];
