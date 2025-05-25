998766fcf2a24b359d23136224e018343e99c60c
Delta-Pion <adi.28per@gmail.com>
2025-05-25 15:34:10 +0530

    Transactions table added and made paginated

--- Client/src/app/all-transactions/all-transactions.component.ts
+++ Client/src/app/all-transactions/all-transactions.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ExpenseService } from '../_services/expense.service';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputIconModule } from 'primeng/inputicon';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-all-transactions',
   standalone: true,
-  imports: [],
+  imports: [TableModule, CommonModule, IconFieldModule, InputTextModule, InputIconModule, TagModule],
   templateUrl: './all-transactions.component.html',
   styleUrl: './all-transactions.component.css'
 })
-export class AllTransactionsComponent {
+export class AllTransactionsComponent implements OnInit {
+  expensesService = inject(ExpenseService);
+  Expenses = this.expensesService.Expenses();
+
+  ngOnInit(): void {
+    if (this.expensesService.Expenses().length === 0) this.loadExpenses();
+
+    console.log(this.expensesService.Expenses());
+  }
+
+  loadExpenses() {
+    this.expensesService.getExpenses()
+  }
 
 }
